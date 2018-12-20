0f3a322a995d8b3270a6ef73ce4128c6bd4f0fd3
FelipeRochaSilvaDev <felipe.rocha.silva254@gmail.com>
2018-12-19 22:00:28 -0200

    Operadores de comparação

---
+++ aula_operadores_04.php
@@ -0,0 +1,24 @@
+<?php 
+
+$a= 55.0;
+
+$b = 55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+var_dump($a == $b); // um sinal de = atribui um dado a variavel, == realiza uma comparação, === verifica se o valor e o tipo são iguais
+
+echo "<br>";
+
+var_dump($a === $b);
+
+echo "<br>";
+
+var_dump($a !== $b);
+?>
---
+++ aula_operadores_05.php
@@ -0,0 +1,8 @@
+<?php 
+
+$a = 35;
+
+$b = 60;
+
+var_dump($a <=> $b);//no space_ship, ele retorna 1 para caso o a for maior que b, 0 para o caso deles serem iguais e -1 para b maior que a 
+?>
