2f2a135680f4005dd5cc58d11d7747b932ed78c2
FelipeRochaSilvaDev <45953143+FelipeRochaSilvaDev@users.noreply.github.com>
2019-01-03 21:38:50 -0200

    Add files via upload

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,14 @@
+//Comentario de uma linha
+console.log('Linha 1')
+/*
+    Comentario de 
+    multiplas linhas 
+*/
+console.log('Linhas multiplas')
+/*
+    *Comentario de
+    *multiplas linhas
+*/
+//Cuidado para não fazer comentarios idiotas
+//TER UM CODIGO LEGIVEL
+//Ter um codigo com bons nomes
---
+++ fundamentos/organização.js
@@ -0,0 +1,8 @@
+console.log("Sentença de Codígo")
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') //Padrão do curso, o qual vou adotar também
+    }
+}
